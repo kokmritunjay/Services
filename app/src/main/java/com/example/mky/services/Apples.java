3d2dc4368f8305c7c444c9ea74d7c439f60e7603
mky <mritunjayk029@gmail.com>
2017-03-24 02:00:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mky/services/Apples.java
@@ -0,0 +1,41 @@
+package com.example.mky.services;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.content.Intent;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
+
+public class Apples extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apples);
+        Intent intent= new Intent(this,MyService.class);
+        startService(intent);
+       /* Button button=(Button) findViewById(R.id.apples_button);
+        button.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+
+                         Intent i = new Intent(this,Bacon.class);
+                        startActivity(i);
+
+                    }
+                }
+        );
+        */
+    }
+    public void onClick(View view){
+        Intent i= new Intent(this,Bacon.class);
+
+        startActivity(i);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/mky/services/MyService.java
@@ -0,0 +1,52 @@
+package com.example.mky.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.util.Log;
+import android.os.HandlerThread;
+
+public class MyService extends Service {
+    public static final String TAG = "om.example.mky.services";
+    public MyService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG,"onStartCommand method called");
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0;i<5;i++) {
+                    Long futureTime = System.currentTimeMillis() + 5000;
+                    while (System.currentTimeMillis() < futureTime) {
+                        synchronized (this) {
+                            try {
+                                wait(futureTime - System.currentTimeMillis());
+                                Log.i(TAG, "Service is doing something");
+                            } catch (Exception e) {
+
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        Thread buckysThread = new Thread(r);
+        buckysThread.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG,"onDestroy method called");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+
+        return null;
+    }
+}
